065c885f513189dc34f7fa3dd11fc56365a192db
Florian Christ <florian.christ@students.fhnw.ch>
2024-05-11 16:40:25 +0200

    almost solved dark mode switch

--- app/components/theme-switch.tsx
+++ app/components/theme-switch.tsx
@@ -1,36 +1,42 @@
 'use client'
-import {useEffect, useState} from "react";
+import {useLayoutEffect, useState} from "react";
 import {Switch} from "@mui/material";
 import {grey} from '@mui/material/colors';
 import {styled} from "@mui/system";
 
 export default function ThemeSwitch() {
-    const [isChecked, setIsChecked] = useState(true);
 
-    useEffect(() => {
-        toggleTheme()
+    useLayoutEffect(() => {
+        let preferredTheme;
+        if (localStorage.getItem('theme') !== null) {
+            preferredTheme = localStorage.getItem('theme');
+        } else {
+            preferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? 'dark'
+                : 'light';
+        }
+        document.documentElement.setAttribute('data-theme', preferredTheme!);
     }, []);
 
-    const setDarkMode = () => {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-
-    const setLightMode = () => {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
+    // TODO: fix this
+    const [isLightMode, setIsLightMode] = useState(
+        document.documentElement.getAttribute('data-theme') === 'light');
 
     const toggleTheme = () => {
-        setIsChecked(!isChecked);
-        isChecked
-            ? setDarkMode()
-            : setLightMode();
+        const newTheme = isLightMode ? 'dark' : 'light';
+        document.documentElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+        setIsLightMode(!isLightMode);
     };
 
     const GreySwitch = styled(Switch)(() => ({
         '& .MuiSwitch-thumb': {
-            backgroundColor: isChecked ? 'rgb(0,0,0)' : 'rgb(255,255,255)'
+            backgroundColor: isLightMode ? 'rgb(0,0,0)' : 'rgb(255,255,255)'
         },
         '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+            backgroundColor: grey[600]
+        },
+        '& .MuiSwitch-track': {
             backgroundColor: grey[600]
         }
     }));
@@ -39,7 +45,7 @@ export default function ThemeSwitch() {
         <button className={'flex fixed bottom-8 left-8'}
         onClick={toggleTheme}>
             <GreySwitch
-                        checked={isChecked}
+                        checked={isLightMode}
                         size={'small'}
             />
             <p className={'text-xs self-center pl-1'}>Lights on/off</p>
