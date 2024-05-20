78006d90a8a9a945e2f2cc093c7bc3f89c9a8604
Florian Christ <florian.christ@students.fhnw.ch>
2024-05-20 09:00:08 +0200

    added animation on start page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
+import "./animations.css";
 import NavBar from "@/app/components/nav-bar";
 import ThemeSwitch from "@/app/components/theme-switch";
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,25 @@ import {ProjectData} from "@/public/data/project-data";
 export default function Home() {
     return (
         <main>
-            <div className='profile-picture'>
-                <Image
-                    src={'/images/passfoto.png'}
-                    alt={'profile-picture'}
-                    width={100}
-                    height={100}>
-                </Image>
+            <div id={'introduction'}>
+                <div id={'profile-picture'} className={'animate animate--fast slideInBottom'}>
+                    <Image
+                        src={'/images/passfoto.png'}
+                        alt={'profile-picture'}
+                        width={100}
+                        height={100}>
+                    </Image>
+                </div>
+                <h1 className={'animate .dur1-1 slideInBottom pb-8'}>Hi, I'm Florian,</h1>
+                <p className={'animate animate--slow .dur1 slideInBottom w-1/2 text-center leading-8'}>
+                    a passionate individual with a diverse background spanning computer sciences,
+                    design and experiences as an draftsman specialising in civil engineering. I thrive
+                    on exploring the intersection of technology and creativity, crafting compelling digital
+                    experiences that resonate with users. With a foundation in computer sciences and civil engineering
+                    I bring a unique perspective to my design process, leveraging technology to push creative
+                    boundaries.
+                </p>
             </div>
-            <h1 className={'pb-8'}>Hi, I'm Florian,</h1>
-            <p className={'w-1/2 text-center leading-8'}>
-                a passionate individual with a diverse background spanning computer sciences,
-                design and experiences as an draftsman specialising in civil engineering. I thrive
-                on exploring the intersection of technology and creativity, crafting compelling digital
-                experiences that resonate with users. With a foundation in computer sciences and civil engineering
-                I bring a unique perspective to my design process, leveraging technology to push creative boundaries.
-            </p>
             <div id={'project-previews'}>
                 {ProjectData.map((project) => (
                         <ProjectPreview
