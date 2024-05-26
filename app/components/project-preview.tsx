c2eecf09cb912e21a70bebdcd31672b5fae89728
Florian Christ <florian.christ@students.fhnw.ch>
2024-05-26 18:25:54 +0200

    added tilt effect to project previews

--- app/components/project-preview.tsx
+++ app/components/project-preview.tsx
@@ -3,6 +3,7 @@ import styles from './ProjectPreview.module.css'
 import Image from "next/image";
 import {useRouter} from "next/navigation";
 import {Technologies} from "@/app/models/technologies";
+import {Tilt} from 'react-tilt';
 
 export default function ProjectPreview(
     {
@@ -21,34 +22,48 @@ export default function ProjectPreview(
 
     const router = useRouter();
 
+    const tiltOptions = {
+        reverse: true,
+        max: 10,
+        perspective: 1000,
+        easing: "cubic-bezier(.03,.98,.52,.99)",
+        scale: "1.01",
+        speed: "1000",
+        transition: true,
+        axis: null,
+        reset: true
+    };
+
     return (
-        <div id={styles['project-preview']}
-             onClick={() => router.push(`work/${id}`)}
-        >
-            <div id={styles['project-infos']} className={'self-center'}>
-                <h2>{title}</h2>
-                <p>{description}</p>
-                <ul id={styles['technologies']}>
-                    {technologies.map(tech => (
-                        <li key={tech.name}>
-                            <Image
-                                src={tech.path}
-                                alt={'technologies'}
-                                width={30}
-                                height={30}>
-                            </Image>
-                        </li>
-                    ))}
-                </ul>
+        <Tilt options={tiltOptions}>
+            <div id={styles['project-preview']}
+                 onClick={() => router.push(`work/${id}`)}
+            >
+                <div id={styles['project-infos']} className={'self-center'}>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <ul id={styles['technologies']}>
+                        {technologies.map(tech => (
+                            <li key={tech.name}>
+                                <Image
+                                    src={tech.path}
+                                    alt={'technologies'}
+                                    width={30}
+                                    height={30}>
+                                </Image>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div id={styles['picture']} className={'self-center'}>
+                    <Image
+                        src={picture}
+                        alt={'project-picture'}
+                        width={1500}
+                        height={400}>
+                    </Image>
+                </div>
             </div>
-            <div id={styles['picture']} className={'self-center'}>
-                <Image
-                    src={picture}
-                    alt={'project-picture'}
-                    width={1500}
-                    height={0}>
-                </Image>
-            </div>
-        </div>
+        </Tilt>
     );
 }
