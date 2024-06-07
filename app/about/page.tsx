adf153a695ddd5ca0319f297f53f512c73b798e7
florian.christ <florian.christ@students.fhnw.ch>
2024-06-07 10:37:44 +0200

    made main- and aboutpage fully responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
   return (
     <main>
       <h1 className={"animate dur600 slideInBottom mt-36 mb-20"}>About me</h1>
-      <ul className={`animate dur800 slideInBottom flex-col content-center pt-10`}>
+      <ul className={`animate dur800 slideInBottom flex-col content-center pt-10 mb-14`}>
         {i18n.About.Timeline.map((waypoint: Waypoint, index: number) => {
           const liClass = index % 2 === 0 ? "pr-52 md:pr-96" : "pl-52 md:pl-96";
           const articleClass = index % 2 === 0 ? "text-right" : "text-left";
@@ -17,9 +17,8 @@ export default function About() {
 
           return (
             <li key={waypoint.description}
-                className={`animate ${durationClass} slideInBottom ${liClass} block relative mb-12`}>
-              <div
-                className={"h-3 w-3 rounded-full bg-accent-color absolute top-0.5 left-1/2 transform -translate-x-1/2"}></div>
+                className={`animate ${durationClass} slideInBottom ${liClass} block relative mb-8 md:mb-12`}>
+              <div className={"h-3 w-3 rounded-full bg-accent-color absolute top-0.5 left-1/2 transform -translate-x-1/2"}></div>
               <article className={`${articleClass} inline-block relative w-44 md:w-72 space-y-2 text-xs md:text-base`}>
                 <h4>{waypoint.time}</h4>
                 <div>{waypoint.description}</div>
@@ -28,7 +27,7 @@ export default function About() {
             </li>
           );
         })}
-        <div className={"absolute top-0 left-1/2 w-1 h-full bg-accent-color transform -translate-x-1/2"}></div>
+        <div className={"absolute top-0 left-1/2 w-1 h-full bg-accent-color transform -translate-x-1/2 rounded-full"}></div>
       </ul>
     </main>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
                  ref={introductionRef}
                  className={"pb-14 mb-20 flex flex-col items-center justify-center"}>
                 <div id={'profile-picture'}
-                     className={"animate dur600 slideInBottom lg:mt-40 lg:mb-16 mt-32 mb-14 flex items-center justify-center w-32 h-32 rounded-full overflow-hidden border border-accent-color"}>
+                     className={"animate dur600 slideInBottom lg:mt-[12%] lg:mb-[7%] mt-32 mb-14 flex items-center justify-center w-32 h-32 rounded-full overflow-hidden border border-accent-color"}>
                     <Image
                         src={'/images/passfoto.png'}
                         alt={'profile-picture'}
@@ -26,8 +26,8 @@ export default function Home() {
                         height={100}>
                     </Image>
                 </div>
-                <h1 className={'animate slideInBottom pb-8 mb-10'}>{i18n.Home.welcome}</h1>
-                <p className={'animate dur1400 slideInBottom w-1/2 text-center leading-8'}>
+                <h1 className={'animate slideInBottom mb-[15%] lg:mb-[8%]'}>{i18n.Home.welcome}</h1>
+                <p className={'animate dur1400 slideInBottom md:w-1/2 text-center leading-6 md:leading-8 lg:mb-[8%] text-[15px] md:text-base'}>
                     {i18n.Home.introduction}
                 </p>
             </div>
