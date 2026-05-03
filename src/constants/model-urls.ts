22fd4d7d82d5ddae07add651d79f8c7d39a62ef6
Florian Christ <florian.christ@students.fhnw.ch>
2026-05-03 15:52:27 +0200

    refactoring

--- src/constants/model-urls.ts
+++ src/constants/model-urls.ts
@@ -1,37 +1,34 @@
 const modelUrls = {
-  table: "/3d_models/hologram_table.glb",
-  excavator: "/3d_models/excavator.glb",
-  macbook: "/3d_models/macbook.glb",
-  computer: "/3d_models/computer.glb",
-  graduationHat: "/3d_models/graduation_hat.glb",
-  computerRound: "/3d_models/computer_round.glb",
-  holoProjector: "/3d_models/holo_projector.glb",
-
   door: "/3d_models/door.glb",
 
   desk: "/3d_models/desk.glb",
   gamingChair: "/3d_models/gaming_chair.glb",
-  kallax: "/3d_models/kallax.glb",
+  magicKeyboard: "/3d_models/magic_keyboard.glb",
+  magicMouse: "/3d_models/magic_mouse.glb",
+  proDisplay: "/3d_models/pro_display.glb",
+  macPro: "/3d_models/mac_pro.glb",
+
   wardrobe: "/3d_models/wardrobe.glb",
+
   tv: "/3d_models/tv.glb",
-  shelf: "/3d_models/floating_shelf.glb",
   glassTable: "/3d_models/glass_table.glb",
+
   bed: "/3d_models/bed.glb",
+
   keyboard: "/3d_models/keyboard.glb",
   stool: "/3d_models/stool.glb",
+
+  kallax: "/3d_models/kallax.glb",
   ps5: "/3d_models/ps5.glb",
   ps5Controller: "/3d_models/ps5_controller.glb",
   books: "/3d_models/books.glb",
   football: "/3d_models/football.glb",
+
+  shelf: "/3d_models/floating_shelf.glb",
   spiderman: "/3d_models/spider-man.glb",
   ezio: "/3d_models/ezio.glb",
   itachi: "/3d_models/itachi.glb",
   obito: "/3d_models/obito.glb",
-
-  magicKeyboard: "/3d_models/magic_keyboard.glb",
-  magicMouse: "/3d_models/magic_mouse.glb",
-  proDisplay: "/3d_models/pro_display.glb",
-  macPro: "/3d_models/mac_pro.glb"
 };
 
 export default modelUrls;
