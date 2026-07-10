4e59b2686721dc4a77deeb02c29d70497ac7d1f7
Florian Christ <florian.christ@students.fhnw.ch>
2026-07-10 11:59:44 +0200

    add welcome overlay

---
+++ src/components/mac_os/programs/safari/content/SafariContent.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+
+export default function SafariContent() {
+    return (
+        <div className='h-full w-full bg-red-300'>
+            in development
+        </div>
+    );
+}
