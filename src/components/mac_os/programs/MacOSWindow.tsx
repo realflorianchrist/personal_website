9f3d2128c2107499d20d6d790b0df0d14ffbe1a6
Florian Christ <florian.christ@students.fhnw.ch>
2026-09-06 17:47:15 +0200

    add glass filter to mac os window

--- src/components/mac_os/programs/MacOSWindow.tsx
+++ src/components/mac_os/programs/MacOSWindow.tsx
@@ -1,32 +1,53 @@
-import React, { ReactNode } from "react";
-import cn from "@/utils/cn";
-import WindowManagementButtons from "@/components/mac_os/programs/buttons/WindowManagementButtons";
-import { useProgramContext } from "@/components/mac_os/programs/Program";
+import WindowManagementButtons from '@/components/mac_os/programs/buttons/WindowManagementButtons';
+import { useProgramContext } from '@/components/mac_os/programs/Program';
+import Glass from '@/components/svg_filters/Glass';
 import { useHandleDrag } from '@/hooks/useHandleDrag';
+import cn from '@/utils/cn';
+import React, { ReactNode } from 'react';
 
 export {
   MacOSWindow,
-  MacOSWindowSidebarContainer,
-  MacOSWindowSidebarHeader,
-  MacOSWindowSidebarContent,
+  MacOSWindowContent,
   MacOSWindowContentContainer,
   MacOSWindowContentHeader,
-  MacOSWindowContent
+  MacOSWindowSidebarContainer,
+  MacOSWindowSidebarContent,
+  MacOSWindowSidebarHeader,
 };
 
+function MacOSWindow({
+  children,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'> & {
+  children?: ReactNode;
+}) {
+  return (
+    <Glass
+      className={cn(
+        'flex w-full h-full bg-popover/97 border-0.5 border-border rounded-3xl overflow-hidden p-2 shadow-xl shadow-black/60',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </Glass>
+  );
+}
 
-function MacOSWindow(
-  {
-    children,
-    className,
-    ...props
-  }: React.ComponentPropsWithoutRef<"div"> & {
-    children?: ReactNode;
-  }) {
+function MacOSWindowSidebarContainer({
+  children,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'> & {
+  children?: ReactNode;
+}) {
   return (
     <div
-      className={cn("flex w-full h-full bg-popover border-0.5 border-border rounded-3xl overflow-hidden p-2 shadow-xl shadow-black/60",
-        className)}
+      className={cn(
+        'flex flex-col border-0.5 border-border rounded-2xl h-full',
+        className,
+      )}
       {...props}
     >
       {children}
@@ -34,43 +55,22 @@ function MacOSWindow(
   );
 }
 
-function MacOSWindowSidebarContainer(
-  {
-    children,
-    className,
-    ...props
-  }: React.ComponentPropsWithoutRef<"div"> & {
-    children?: ReactNode;
-  }) {
-  return (
-    <div
-      className={cn("flex flex-col border-0.5 border-border rounded-2xl h-full",
-        className)}
-      {...props}
-    >
-      {children}
-    </div>
-  );
-}
-
-function MacOSWindowSidebarHeader(
-  {
-    children,
-    className,
-    onPointerDown,
-    onPointerUp,
-    ...props
-  }: React.ComponentPropsWithoutRef<"div"> & {
-    children?: ReactNode;
-  }) {
-
+function MacOSWindowSidebarHeader({
+  children,
+  className,
+  onPointerDown,
+  onPointerUp,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'> & {
+  children?: ReactNode;
+}) {
   const { programId } = useProgramContext();
 
   const { handleDrag } = useHandleDrag(programId);
 
   return (
     <div
-      className={cn("h-10 flex p-2", className)}
+      className={cn('h-10 flex p-2', className)}
       onPointerDown={(e) => {
         onPointerDown?.(e);
 
@@ -91,17 +91,16 @@ function MacOSWindowSidebarHeader(
   );
 }
 
-function MacOSWindowSidebarContent(
-  {
-    children,
-    className,
-    ...props
-  }: React.ComponentPropsWithoutRef<"div"> & {
-    children?: ReactNode;
-  }) {
+function MacOSWindowSidebarContent({
+  children,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'> & {
+  children?: ReactNode;
+}) {
   return (
     <div
-      className={cn("flex-1 w-56 px-2 text-sm overflow-auto", className)}
+      className={cn('flex-1 w-56 px-2 text-sm overflow-auto', className)}
       {...props}
     >
       {children}
@@ -109,42 +108,39 @@ function MacOSWindowSidebarContent(
   );
 }
 
-function MacOSWindowContentContainer(
-  {
-    children,
-    className,
-    ...props
-  }: React.ComponentPropsWithoutRef<"div"> & {
-    children?: ReactNode;
-  }) {
+function MacOSWindowContentContainer({
+  children,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'> & {
+  children?: ReactNode;
+}) {
   return (
-    <div
-      className={cn("flex flex-col flex-1", className)}
-      {...props}
-    >
+    <div className={cn('flex flex-col flex-1', className)} {...props}>
       {children}
     </div>
   );
 }
 
-function MacOSWindowContentHeader(
-  {
-    children,
-    className,
-    onPointerDown,
-    onPointerUp,
-    ...props
-  }: React.ComponentPropsWithoutRef<"div"> & {
-    children?: ReactNode;
-  }) {
-
+function MacOSWindowContentHeader({
+  children,
+  className,
+  onPointerDown,
+  onPointerUp,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'> & {
+  children?: ReactNode;
+}) {
   const { programId } = useProgramContext();
 
   const { handleDrag } = useHandleDrag(programId);
 
   return (
     <div
-      className={cn("h-10 flex items-center px-3 gap-2 text-lg", className)}
+      className={cn(
+        'h-10 flex items-center px-3 gap-2 text-lg z-10',
+        className,
+      )}
       onPointerDown={(e) => {
         onPointerDown?.(e);
 
@@ -166,17 +162,16 @@ function MacOSWindowContentHeader(
   );
 }
 
-function MacOSWindowContent(
-  {
-    children,
-    className,
-    ...props
-  }: React.ComponentPropsWithoutRef<"div"> & {
-    children?: ReactNode;
-  }) {
+function MacOSWindowContent({
+  children,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'> & {
+  children?: ReactNode;
+}) {
   return (
     <div
-      className={cn("flex flex-1 p-3 text-sm overflow-auto", className)}
+      className={cn('flex flex-1 p-3 text-sm overflow-auto', className)}
       {...props}
     >
       {children}
